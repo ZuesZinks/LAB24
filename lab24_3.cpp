51bbf8b2ff19fe60b63bcb6a58c7fc72c90434d0
ZuesZinks <non_13491@hotmail.com>
2025-03-04 12:34:50 +0700

    lab24.3

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -3,6 +3,20 @@
 using namespace std;
 
 //Write function count() here.
+int count(int data[], int size){
+	bool check[100] = {false};
+	int sum = 0;
+	for(int i = 0; i < size; i++){
+		if(!check[data[i]]){
+			check[data[i]] = true;
+			sum++;
+		}
+		else{
+			continue;
+		}
+	}
+	return sum;
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
